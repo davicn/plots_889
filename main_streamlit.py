ec1cc959fb79a7e4bd6be7e43c1260701b9426be
Davi Nascimento <davi.nascimento777@gmail.com>
2022-12-06 18:29:22 -0300

    Update main_streamlit.py

--- main_streamlit.py
+++ main_streamlit.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-# import matplotlib.pyplot as plt 
-# import plotly.express as px
+import matplotlib.pyplot as plt 
+import plotly.express as px
 
 url = 'https://raw.githubusercontent.com/albertsl/datasets/master/popularidad/temporal.csv'
 
