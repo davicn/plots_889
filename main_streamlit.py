c8f17e3ad3f4724073c9d2785de001e44420b65e
Davi Nascimento <davi.nascimento777@gmail.com>
2022-12-06 18:29:53 -0300

    Update main_streamlit.py

--- main_streamlit.py
+++ main_streamlit.py
@@ -30,15 +30,15 @@ elif option=='Bar':
     st.bar_chart(data=df,
             x='Mes')
 
-# elif option=='Pie':
-#     x = ['a','b','c','d','e','f']
-#     y = [45,67,32,20,23,78]
+elif option=='Pie':
+    x = ['a','b','c','d','e','f']
+    y = [45,67,32,20,23,78]
 
-#     fig = px.pie(
-#         df,
-#         values='data science',
-#         names='categorical',
-#         # title='Pizza Frango com Catupiry',
-#         # color_discrete_sequence= px.colors.sequential.Oranges
-#     )
-#     st.plotly_chart(fig)
+    fig = px.pie(
+        df,
+        values='data science',
+        names='categorical',
+        # title='Pizza Frango com Catupiry',
+        # color_discrete_sequence= px.colors.sequential.Oranges
+    )
+    st.plotly_chart(fig)
